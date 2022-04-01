2501385a5d52eea7339209d063c3513c8fbe61ee
KISHORE-D0707 <83343114+KISHORE-D0707@users.noreply.github.com>
2022-04-01 10:34:09 +0530

    Add files via upload

---
+++ company/CompanyInfo.java
@@ -0,0 +1,23 @@
+package org.company;
+
+public class CompanyInfo{
+	private void companyName() {
+		System.out.println("companyname");
+		
+	}
+	private void companyid() {
+		System.out.println("5118");
+	
+	}
+	private void companyAddress() {
+		System.out.println("omr");
+	}
+	public static void main( String[]args) {
+		CompanyInfo n = new CompanyInfo();
+		n.companyName();
+		n.companyid();
+		n.companyAddress();
+		
+	}
+
+}
